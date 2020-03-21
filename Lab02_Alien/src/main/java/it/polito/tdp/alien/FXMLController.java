2d19dce0d3dea6a1404d37d8da86323715ec9653
Riccardo <Riccardo@DESKTOP-U0PF1S3.home-life.hub>
2020-03-21 12:44:14 +0100

    Grafica sistemata

--- Lab02_Alien/src/main/java/it/polito/tdp/alien/FXMLController.java
+++ Lab02_Alien/src/main/java/it/polito/tdp/alien/FXMLController.java
@@ -2,10 +2,10 @@ package it.polito.tdp.alien;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class FXMLController {
 
@@ -14,27 +14,27 @@ public class FXMLController {
 
     @FXML
     private URL location;
-    
+
     @FXML
-    private Button btnTranslate;
-    
+    private TextField inputText;
+
     @FXML
-    private Button btnReset;
+    private TextArea outputText;
+
+    @FXML
+    void doClearText(ActionEvent event) {
+
+    }
 
     @FXML
     void doTranslate(ActionEvent event) {
-    	// TODO - add the button and complete this    	
+
     }
-    
-    
-    @FXML
-    void doReset(ActionEvent event) {
-    	// TODO - add the button and complete this 
-    }
-    
-    
+
     @FXML
     void initialize() {
+        assert inputText != null : "fx:id=\"inputText\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert outputText != null : "fx:id=\"outputText\" was not injected: check your FXML file 'Scene.fxml'.";
 
     }
 }
